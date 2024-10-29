c11c56a865618694ed96809facb1cffd2b4a9b42
Julian Berman <Julian@GrayVines.com>
2024-10-29 07:24:56 +0000

    bowtie 2024.10.6

--- bowtie.rb
+++ bowtie.rb
@@ -4,8 +4,8 @@ class Bowtie < Formula
   desc "Meta-validator for the JSON Schema Specifications"
   homepage "https://bowtie-json-schema.github.io/bowtie/"
   url "https://github.com/bowtie-json-schema/bowtie.git",
-      tag:      "v2024.10.5",
-      revision: "aaedd5a2fcd4b4dc360b9b286f8e4224a652a1a5"
+      tag:      "v2024.10.6",
+      revision: "57d1442adfa1039a4707eff5bc90b9bd6f68870f"
   license "MIT"
 
   livecheck do
